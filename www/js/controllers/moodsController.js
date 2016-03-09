21f7306eab98d7f9a2dd478b6eba67b73910070e
Cameron Wheeler <cameron@scelos.com>
2016-03-09 10:56:00 -0500

    More datastore work

--- www/js/controllers/moodsController.js
+++ www/js/controllers/moodsController.js
@@ -1,3 +1,7 @@
 app.controller('moodsController', function($scope, datastore) {
-	
+	datastore.addContainer('test', {
+		name: datastore.types.String
+	});
+
+	datastore.save("test", {name: "Cameron"});
 });
--- www/js/services/datastore.js
+++ www/js/services/datastore.js
@@ -1,31 +1,108 @@
 app.service('datastore', function($window) {
+	//Model Types
+	this.types = {
+		String: 'string',
+		Number: 'number'
+	}
+
+	//Set an object in local storage
+	function setObject(key, obj) {
+		$window.localStorage[key] = JSON.stringify(obj);
+	}
+
+	//Fetch an object
+	function getObject(key) {
+		if ($window.localStorage[key]) {
+			return JSON.parse($window.localStorage[key]);
+		} else {
+			return undefined;
+		}
+	}
+
+	//Add a new container with the given model
+	this.addContainer = function(name, model) {
+		var models = getObject("models");
+
+		if (!name || name == "") {
+			throw new Error("Must have a valid model name to add a new container");
+		}
+
+		if (!model || typeof model != 'object') {
+			throw new Error("Must have a valid model to add a new continer");
+		}
+
+		if (!models) {
+			setObject("models", {});
+			models = {};
+		}
+
+		models[name] = model;
+		setObject("models", models);
+
+		if (!getObject(name)) {
+			setObject(name, []);
+		}
+	}
+
+	//Remove an entire container
+	this.removeContainer = function(container, document) {
+		if (!container || container == "" || !$window.localStorage[container]) {
+			throw new Error("Must have a valid container name to save to");
+		}
+
+		delete $window.localStorage[container];
+	};
+
 	//Save a document to a container. If the container does not exist, make a new one.
 	//If an id is given, an attempt is made to replace the old existing document
 	this.save = function(container, document, id) {
-		if (!container || container == "") {
-			throw new Error("Must have a valid container name to save to");
+		var model = getObject("models")[container];
+
+		if (!container || container == "" || !$window.localStorage[container]) {
+			throw new Error("Must have a valid container to save to");
 		}
 
 		if (!document || document == "") {
 			throw new Error("Must have a valid document to save");
 		}
 
-		if (!$window.localStorage[container]) {
-			$window.localStorage[container] = [];
+		if (!model) {
+			throw new Error("Containers must have been modeled prior to saving in them");
+		}
+
+		var docKeys = Object.keys(document);
+		var modKeys = Object.keys(model);
+		var containerObjs = getObject(container);
+
+		if (docKeys.length != modKeys.length) {
+			throw new Error("Documents in container must have same keys as model for container");
+		}
+
+		for (var i = 0; i < docKeys.length; i++) {
+			if (!model[docKeys[i]]) {
+				throw new Error("Document has unknown key '" + docKeys[i] + "'");
+			}
+
+			if (typeof document[docKeys[i]] != model[docKeys[i]]) {
+				throw new Error("Key mismatch, typeof '" + docKeys[i] + "' is not equal to type '" + model[docKeys[i]] + "'");
+			}
 		}
 
 		if (id) {
-			if (!$window.localStorage[container][id]) {
+			if (!getObject(container)[id]) {
 				throw new Error("The id " + id + " does not exist in the container '" + container + "'");
 			} else {
-				$window.localStorage[container][id] = document;
+				containerObjs[id] = document;
+				setObject(container, containerObjs);
 				document.id = id;
 
 				return document;
 			}
 		} else {
-			$window.localStorage[container].push(document);
-			document.id = $window.localStorage[container].length-1;
+			console.log("looks liek", containerObjs);
+			containerObjs.push(document);
+			setObject(container, containerObjs);
+			document.id = containerObjs.length-1;
 
 			return document;
 		}
@@ -41,16 +118,14 @@ app.service('datastore', function($window) {
 			throw new Error("Must have a valid document id to remove");
 		}
 
-		$window.localStorage[container][id] = undefined;
-	};
+		var containerObjs = getObject(container);
 
-	//Remove an entire container
-	this.removeContainer = function(container, document) {
-		if (!container || container == "" || !$window.localStorage[container]) {
-			throw new Error("Must have a valid container name to save to");
+		if (id < containerObjs.length) {
+			containerObjs[id] = undefined;
+			return true;
+		} else {
+			return false;
 		}
-
-		delete $window.localStorage[container];
 	};
 
 	//Fetch a single entity from a container
@@ -63,7 +138,13 @@ app.service('datastore', function($window) {
 			throw new Error("Must have a valid document id to fetch");
 		}
 
-		return $window.localStorage[container][id];
+		var containerObjs = getObject(container);
+
+		if (id < containerObjs.length) {
+			return containerObjs[id];
+		} else {
+			
+		}		
 	};	
 
 	//Fetch all entities from a container
