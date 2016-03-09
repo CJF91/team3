863500b26777baec12bc5b20a0ec74ce0b24cd0f
Cameron Wheeler <cameron@scelos.com>
2016-03-09 08:25:21 -0500

    Added correct tabs and routing information

--- www/js/app.js
+++ www/js/app.js
@@ -25,61 +25,62 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
+  $stateProvider.state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.moods', {
+    url: '/moods',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
+      'tab-moods': {
+        templateUrl: 'templates/tab-moods.html',
         controller: 'DashCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.patterns', {
+    url: '/patterns',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-patterns': {
+        templateUrl: 'templates/tab-patterns.html',
+        controller: 'DashCtrl'
       }
     }
-  });
+  })
 
-  // if none of the above states are matched, use this as the fallback
+  .state('tab.help', {
+    url: '/patterns',
+    views: {
+      'tab-help': {
+        templateUrl: 'templates/tab-help.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.progress', {
+    url: '/progress',
+    views: {
+      'tab-progress': {
+        templateUrl: 'templates/tab-progress.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.preferences', {
+    url: '/preferences',
+    views: {
+      'tab-preferences': {
+        templateUrl: 'templates/tab-preferences.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  
+
   $urlRouterProvider.otherwise('/tab/dash');
-
 });
