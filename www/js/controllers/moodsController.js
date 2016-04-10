ff70498b0ca446036c7df206c50cad61cd90bf40
SeihakRithy Muth <Rithy58@gmail.com>
2016-04-10 10:57:15 -0400

    changes the subheader

--- www/js/controllers/moodsController.js
+++ www/js/controllers/moodsController.js
@@ -2,7 +2,16 @@ app.controller('moodsController', function($scope, $ionicActionSheet, datastore)
   $scope.moods = [
     {mood: 'Happy', time: 1},
     {mood:'Sad', time: 2},
-    {mood:'Angry', time: 3}];
+    {mood:'Angry', time: 3},
+    {mood:'Angry', time: 4},
+    {mood:'Angry', time: 5},
+    {mood:'Angry', time: 6},
+    {mood:'Angry', time: 7},
+    {mood:'Angry', time: 8},
+    {mood:'Angry', time: 9},
+    {mood:'Angry', time: 10},
+    {mood:'Angry', time: 11},
+    {mood:'Angry', time: 12}];
   $scope.options = {
     showDelete: false,
     canSwipe: true
