2b834884948d46e39ec9ea9a61361120ef0eb762
Cameron Wheeler <cameron@scelos.com>
2016-04-14 23:42:56 -0400

    Get Help UI

--- www/js/controllers/help/strategyController.js
+++ www/js/controllers/help/strategyController.js
@@ -1,3 +1,20 @@
-app.controller('strategyController', function($scope, $stateParams) {
+app.controller('strategyController', function($scope, $stateParams, $timeout) {
+	var feelings = ['Worse', 'Bit Worse', 'No Better', 'Meh', 'Netural', 'Bit Better', 'Better', 'Lot Better', 'Great', 'Awesome'];
+
 	$scope.mood = $stateParams.mood;
+	$scope.effectLevel = 'circle1';
+	$scope.effectFeeling = feelings[0];
+
+	$scope.changeEffect = function() {
+		var level = parseInt($scope.effectLevel.replace("circle", "")) + 1;
+		if (level > 10) level = 1;
+
+		$scope.effectFeeling = feelings[level-1];
+
+		$scope.effectLevel = "circle-scale circle" + level;
+
+		$timeout(function() {
+			$scope.effectLevel = "circle" + level;
+		}, 275);
+	};
 });
