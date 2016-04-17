31545c34ff1c5004e22f8eb5dff1bb6761d12268
Cameron Wheeler <cameron@scelos.com>
2016-04-17 12:44:43 -0400

    Made strategies have multipul moods

--- www/js/controllers/helpController.js
+++ www/js/controllers/helpController.js
@@ -3,9 +3,9 @@ app.controller('helpController', function($scope, datastore, $state) {
 		$state.go('tab.help-strategy', {mood: mood});
 	};
 
-	//mock data container
+	//Copeing Strategy Container
 	datastore.addContainer('Strategy', {
-		'mood' : datastore.types.String,
+		'moods' : datastore.types.Array,
 		'title' : datastore.types.String,
 		'content' : datastore.types.String,
 		'effect' : datastore.types.Number,
@@ -14,32 +14,32 @@ app.controller('helpController', function($scope, datastore, $state) {
 	//effectList
 	$scope.effectList =  ['Worse', 'Bit Worse', 'No Better', 'Meh', 'Netural', 'Bit Better', 'Better', 'Lot Better', 'Great', 'Awesome'];
 
-	//mock data
+	//Mock Copeing Strategies
 	datastore.save('Strategy', {
-		'mood': 'Happy',
+		'mood': ['Happy', 'Scared'],
 		'title': 'Deep Breathing',
-		'content': 'I guess you can like breath and stuff. If your weird and have a problem with being happy that is. Which I have no idea why that would be. But I guess we have to help you with your happiness problems. First world problems am I right. Like literally your too happy, damn, I wish I had your problems',
+		'content': 'Sometimes breathing deeply and focusing on what\'s important can help you calm down. Try to stop what your doing and focus on your breathing. Breath deeply three or more times to calm down.',
 		'effect': 0,
 	});
 
 	datastore.save('Strategy', {
-		'mood': 'Excited',
+		'mood': ['Excited', 'Happy'],
 		'title': 'Do what interests you the most',
 		'content': "You might feel like nothing excites you. Like you don't have anything. But I'm sure there is something in this wide world of possibilities that is more interesting than other things. You don't have to be wildly in love – just moderately curious. Start there.",
 		'effect': 6,
 	});
 
 	datastore.save('Strategy', {
-		'mood': 'Angry',
+		'mood': ['Angry', 'Tender'],
 		'title': 'Take a timeout',
 		'content': "Timeouts aren't just for kids. Give yourself short breaks during times of the day that tend to be stressful. A few moments of quiet time might help you feel better prepared to handle what's ahead without getting irritated or angry.",
 		'effect': 5,
 	});
 
 	datastore.save('Strategy', {
-		'mood': 'Sad',
+		'mood': ['Sad'],
 		'title': 'Smile and laugh more',
-		'content': "Smiling and lauging – even faking it – will make you feel happier. I'm not encouraging false laughs here, but if you are feeling low and uninspired, try watching a comedy or just smiling to strangers. This totally works! You might feel goofy, but if that can make you laugh of yourself – even better!",
+		'content': "Smiling and laughing – even faking it – will make you feel happier. I'm not encouraging false laughs here, but if you are feeling low and uninspired, try watching a comedy or just smiling to strangers. This totally works! You might feel goofy, but if that can make you laugh of yourself – even better!",
 		'effect': 5,
 	});
 });
