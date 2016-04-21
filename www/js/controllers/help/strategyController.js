3ad93564feb8b2b7163af1db4c740688ee8d625a
Cameron Wheeler <cameron@scelos.com>
2016-04-21 01:07:05 -0400

    Added percent effect

--- www/js/controllers/help/strategyController.js
+++ www/js/controllers/help/strategyController.js
@@ -1,17 +1,48 @@
-app.controller('strategyController', function($scope, $stateParams, $timeout) {
+app.controller('strategyController', function($scope, $stateParams, $timeout, datastore, $rootScope) {
 	var feelings = ['Worse', 'Bit Worse', 'No Better', 'Meh', 'Netural', 'Bit Better', 'Better', 'Lot Better', 'Great', 'Awesome'];
+	var record = -1; 
 
 	$scope.mood = $stateParams.mood;
 	$scope.effectLevel = 'circle1';
 	$scope.effectFeeling = feelings[0];
 
 	$scope.strategy = $stateParams.strategy;
+	$scope.percentHelps = 6.4;
+
+	var events = datastore.find('StrategyEvent', 'mood', $stateParams.mood);
+	var helped = 0;
+
+	for (var i = 0; i < events.length; i++) {
+		if (events[i].effect > 4) {
+			helped++;
+		}
+	}
+
+	$scope.percentHelps = parseInt((helped / events.length) * 100, 10);
 
 	$scope.changeEffect = function() {
 		var level = parseInt($scope.effectLevel.replace("circle", "")) + 1;
 		if (level > 10) level = 1;
 
 		$scope.effectFeeling = feelings[level-1];
+
+		if (record == -1) {
+			var saved = datastore.save('StrategyEvent', {
+				strategy: $stateParams.strategy.id,
+				effect: level-1,
+				mood: $stateParams.mood
+			});
+
+			record = saved.id;
+		} else {
+			datastore.save('StrategyEvent', {
+				strategy: $stateParams.strategy.id,
+				effect: level-1,
+				mood: $stateParams.mood
+			}, record);
+		}
+
+		$rootScope.$broadcast('refreshGetHelp');
 
 		$scope.effectLevel = "circle-scale circle" + level;
 
--- www/js/controllers/helpController.js
+++ www/js/controllers/helpController.js
@@ -1,4 +1,7 @@
-app.controller('helpController', function($scope, datastore, $state) {
+app.controller('helpController', function($scope, datastore, $state, $rootScope) {
+	$scope.feelings = {'Happy': 0, 'Excited' : 0, 'Tender': 0, 'Scared': 0, 'Sad': 0, 'Angry': 0};
+
+	//Select a strategy
 	$scope.showStrategy = function(mood) {
 		var applicable = [];
 		var strategies = datastore.getAll('Strategy');
@@ -12,12 +15,29 @@ app.controller('helpController', function($scope, datastore, $state) {
 		$state.go('tab.help-strategy', {mood: mood, strategy: applicable[Math.ceil(Math.random() * applicable.length - 1)]});
 	};
 
-	//Copeing Strategy Container
+	//Add containers
 	datastore.addContainer('Strategy', {
 		'moods' : datastore.types.Array,
 		'title' : datastore.types.String,
-		'content' : datastore.types.String,
+		'content' : datastore.types.String
+	});
+
+	datastore.addContainer('StrategyEvent', {
+		'strategy' : datastore.types.Number,
 		'effect' : datastore.types.Number,
+		'mood': datastore.types.String
+	});
+
+	//Update the number of times helped per mood
+	for (feeling in $scope.feelings) {
+		$scope.feelings[feeling] = datastore.find('StrategyEvent', 'mood', feeling).length;
+	}
+
+	//Add a hook to refresh later
+	$rootScope.$on('refreshGetHelp', function() {
+		for (feeling in $scope.feelings) {
+			$scope.feelings[feeling] = datastore.find('StrategyEvent', 'mood', feeling).length;
+		}
 	});
 
 	$scope.effectList =  ['Worse', 'Bit Worse', 'No Better', 'Meh', 'Netural', 'Bit Better', 'Better', 'Lot Better', 'Great', 'Awesome'];
@@ -27,29 +47,25 @@ app.controller('helpController', function($scope, datastore, $state) {
 		datastore.save('Strategy', {
 			'moods': ['Happy', 'Scared'],
 			'title': 'Deep Breathing',
-			'content': 'Sometimes breathing deeply and focusing on what\'s important can help you calm down. Try to stop what your doing and focus on your breathing. Breath deeply three or more times and try to relax.',
-			'effect': 0,
+			'content': 'Sometimes breathing deeply and focusing on what\'s important can help you calm down. Try to stop what your doing and focus on your breathing. Breath deeply three or more times and try to relax.'
 		});
 
 		datastore.save('Strategy', {
 			'moods': ['Excited', 'Happy'],
 			'title': 'Something Interesting',
-			'content': "You might feel like nothing excites you. Like you don't have anything. But I'm sure there is something in this wide world of possibilities that is more interesting than other things. You don't have to be wildly in love – just moderately curious. Start there by doing something interesting.",
-			'effect': 6,
+			'content': "You might feel like nothing excites you. Like you don't have anything. But I'm sure there is something in this wide world of possibilities that is more interesting than other things. You don't have to be wildly in love – just moderately curious. Start there by doing something interesting."
 		});
 
 		datastore.save('Strategy', {
 			'moods': ['Angry', 'Tender'],
 			'title': 'Take a timeout',
-			'content': "Timeouts aren't just for kids. Give yourself short breaks during times of the day that tend to be stressful. A few moments of quiet time might help you feel better prepared to handle what's ahead without getting irritated or angry.",
-			'effect': 5,
+			'content': "Timeouts aren't just for kids. Give yourself short breaks during times of the day that tend to be stressful. A few moments of quiet time might help you feel better prepared to handle what's ahead without getting irritated or angry."
 		});
 
 		datastore.save('Strategy', {
 			'moods': ['Sad'],
 			'title': 'Smile and Laugh',
-			'content': "Smiling and laughing – even faking it – will make you feel happier. I'm not encouraging false laughs here, but if you are feeling low and uninspired, try watching a comedy or just smiling to strangers. This totally works! You might feel goofy, but if that can make you laugh of yourself – even better!",
-			'effect': 5,
+			'content': "Smiling and laughing – even faking it – will make you feel happier. I'm not encouraging false laughs here, but if you are feeling low and uninspired, try watching a comedy or just smiling to strangers. This totally works! You might feel goofy, but if that can make you laugh of yourself – even better!"
 		});
 	}
 });
