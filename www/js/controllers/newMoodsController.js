67d4a376d9b8a23fcefeca697b1f44d462d0cbaa
SeihakRithy Muth <Rithy58@gmail.com>
2016-04-29 11:45:22 -0400

    added new data

--- www/js/controllers/newMoodsController.js
+++ www/js/controllers/newMoodsController.js
@@ -20,8 +20,20 @@ app.controller('newMoodsController', function($scope, datastore, $location, $ion
 
     $scope.modalData = {
         header: "Header",
-        items: [{name: "item 1"}, {name: "item2"}],
+        items: [{
+            name: "item 1"
+        }, {
+            name: "item2"
+        }],
         id: 0
+    };
+
+    $scope.modalData2 = {
+        header: "Header",
+        input: "",
+        name: "",
+        isMood: false,
+        type: ""
     };
 
     datastore.upsert("Mood", {
@@ -56,6 +68,13 @@ app.controller('newMoodsController', function($scope, datastore, $location, $ion
         $scope.modal = modal;
     });
 
+    $ionicModal.fromTemplateUrl("templates/tab-moods-new-modal2.html", {
+        scope: $scope,
+        animation: "slide-in-up"
+    }).then(function(modal) {
+        $scope.modal2 = modal;
+    });
+
     $scope.openModal = function(id) {
         $scope.modal.show();
 
@@ -70,20 +89,48 @@ app.controller('newMoodsController', function($scope, datastore, $location, $ion
                 $scope.modalData.id = 2;
                 $scope.modalData.items = datastore.getAll("Trigger");
                 break;
-						case 3:
-		            $scope.modalData.header = "Select Behavior";
-		            $scope.modalData.id = 3;
-		            $scope.modalData.items = datastore.getAll("Behavior");
-		            break;
-						case 4:
-							$scope.modalData.header = "Select Belief";
-							$scope.modalData.id = 4;
-							$scope.modalData.items = datastore.getAll("Belief");
-							break;
+            case 3:
+                $scope.modalData.header = "Select Behavior";
+                $scope.modalData.id = 3;
+                $scope.modalData.items = datastore.getAll("Behavior");
+                break;
+            case 4:
+                $scope.modalData.header = "Select Belief";
+                $scope.modalData.id = 4;
+                $scope.modalData.items = datastore.getAll("Belief");
+                break;
             default:
                 $scope.modalData.header = "Error";
         };
     };
+
+    $scope.openModal2 = function() {
+        $scope.modal2.show();
+        switch ($scope.modalData.id) {
+            case 1:
+                $scope.modalData2.header = "Enter New Mood";
+                $scope.modalData2.input = "Mood: ";
+                $scope.modalData2.isMood = true;
+                break;
+            case 2:
+                $scope.modalData2.header = "Enter New Trigger";
+                $scope.modalData2.input = "Trigger: ";
+                $scope.modalData2.isMood = false;
+                break;
+            case 3:
+                $scope.modalData2.header = "Enter New Behavior";
+                $scope.modalData2.input = "Behavior: ";
+                $scope.modalData2.isMood = false;
+                break;
+            case 4:
+                $scope.modalData2.header = "Enter New Belief";
+                $scope.modalData2.input = "Belief: ";
+                $scope.modalData2.isMood = false;
+                break;
+            default:
+                $scope.modalData.header = "Error";
+        };
+    }
 
     $scope.itemSelected = function(item) {
         switch ($scope.modalData.id) {
@@ -91,29 +138,60 @@ app.controller('newMoodsController', function($scope, datastore, $location, $ion
                 $scope.doc.mood = item.name;
                 newLog.mood = item.id;
                 $scope.modal.hide();
-								break;
-						case 2:
-								$scope.doc.trigger = item.name;
-								newLog.trigger = item.id;
-								$scope.modal.hide();
-								break;
-						case 3:
-								$scope.doc.behavior = item.name;
-								newLog.behavior = item.id;
-								$scope.modal.hide();
-								break;
-						case 4:
-								$scope.doc.belief = item.name;
-								newLog.belief = item.id;
-								$scope.modal.hide();
-								break;
+                break;
+            case 2:
+                $scope.doc.trigger = item.name;
+                newLog.trigger = item.id;
+                $scope.modal.hide();
+                break;
+            case 3:
+                $scope.doc.behavior = item.name;
+                newLog.behavior = item.id;
+                $scope.modal.hide();
+                break;
+            case 4:
+                $scope.doc.belief = item.name;
+                newLog.belief = item.id;
+                $scope.modal.hide();
+                break;
             default:
-								$scope.modal.hide();
+                $scope.modal.hide();
+        };
+    }
+
+    $scope.save = function() {
+        switch ($scope.modalData.id) {
+            case 1:
+                datastore.upsert("Mood", {name: $scope.modalData2.name, type: parseInt($scope.modalData2.type)}, "name");
+                $scope.modalData.items = datastore.getAll("Mood");
+                $scope.modal2.hide();
+                break;
+            case 2:
+                datastore.upsert("Trigger", {name: $scope.modalData2.name}, "name")
+                $scope.modalData.items = datastore.getAll("Trigger");
+                $scope.modal2.hide();
+                break;
+            case 3:
+                datastore.upsert("Behavior", {name: $scope.modalData2.name}, "name")
+                $scope.modalData.items = datastore.getAll("Behavior");
+                $scope.modal2.hide();
+                break;
+            case 4:
+                datastore.upsert("Belief", {name: $scope.modalData2.name}, "name")
+                $scope.modalData.items = datastore.getAll("Belief");
+                $scope.modal2.hide();
+                break;
+            default:
+                $scope.modal2.hide();
         };
     }
 
     $scope.closeModal = function() {
         $scope.modal.hide();
+    }
+
+    $scope.closeModal2 = function() {
+        $scope.modal2.hide();
     }
 
     $scope.chooseMood = function(mood) {
@@ -144,5 +222,6 @@ app.controller('newMoodsController', function($scope, datastore, $location, $ion
 
     $scope.$on("$destroy", function() {
         $scope.modal.remove();
+        $scope.modal2.remove();
     });
 });
