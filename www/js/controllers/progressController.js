d2ed9e14436dfce6af54062566372e3e3a1b90a2
James Sullivan <james@scelos.com>
2016-05-02 00:08:32 -0400

    Search Moods can now take in Dates as params

--- www/js/controllers/progressController.js
+++ www/js/controllers/progressController.js
@@ -8,8 +8,8 @@ var beliefs = datastore.getAll('Belief');
 var behavior = datastore.getAll('Behavior');
 var trigger = datastore.getAll('Trigger');
 
-var findRelevantMoods = function (mood,belief,behavior,trigger) {
-	if (!mood && !belief && !behavior && !trigger){
+var searchMoods = function (mood,belief,behavior,trigger,beforeDate,afterDate) {
+	if (!mood && !belief && !behavior && !trigger && !beforeDate && !afterDate){
 		return AllMoods;
 	}
 	var sorted = JSON.parse((JSON.stringify(AllMoods))); // Deep Copy
@@ -33,16 +33,27 @@ var findRelevantMoods = function (mood,belief,behavior,trigger) {
 			return trigger.indexOf(elem.trigger) != -1;
 		})
 	}
-
-
+	if(beforeDate && afterDate){
+		sorted = sorted.filter(function (elem) {
+			return moment(elem.date).isBetween(beforeDate,afterDate, null, '[]');
+		})
+	} else if (beforeDate) {
+		sorted = sorted.filter(function (elem) {
+			return moment(elem.date).isSameOrBefore(beforeDate);
+		})
+	} else if (afterDate){
+		sorted = sorted.filter(function (elem) {
+			return moment(elem.date).isSameOrAfter(afterDate);
+		})
+	}
 	return sorted;
 
 }
 console.log(moods);
 console.log(moods[0]);
-console.log(findRelevantMoods([1,2], [2],[2],[0]))
+console.log(searchMoods([1,2], [2],[2],[0]))
 
-
+console.log(moment(moods[0].date));
 
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
