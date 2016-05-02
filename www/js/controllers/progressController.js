d8e098b2bf6002df1b896cba791fe638cffff01f
James Sullivan <james@scelos.com>
2016-05-01 23:19:25 -0400

    Added a Filtering to Track Moods

--- www/js/controllers/progressController.js
+++ www/js/controllers/progressController.js
@@ -1,6 +1,47 @@
-app.controller('progressController', function($scope) {
+app.controller('progressController', function($scope,datastore) {
 
 // Test Data From the chartjs website
+var AllMoods = datastore.getAll('MoodEvent');
+
+var moods = datastore.getAll('Mood');
+var beliefs = datastore.getAll('Belief');
+var behavior = datastore.getAll('Behavior');
+var trigger = datastore.getAll('Trigger');
+
+var findRelevantMoods = function (mood,belief,behavior,trigger) {
+	if (!mood && !belief && !behavior && !trigger){
+		return AllMoods;
+	}
+	var sorted = JSON.parse((JSON.stringify(AllMoods))); // Deep Copy
+	if (mood){
+		sorted = sorted.filter(function (elem) {
+			return mood.indexOf(elem.mood) != -1;
+		})
+	}
+	if (belief){
+		sorted = sorted.filter(function (elem) {
+			return belief.indexOf(elem.belief) != -1;
+		})
+	}
+	if (behavior){
+		sorted = sorted.filter(function (elem) {
+			return behavior.indexOf(elem.behavior) != -1;
+		})
+	}
+	if (trigger){
+		sorted = sorted.filter(function (elem) {
+			return trigger.indexOf(elem.trigger) != -1;
+		})
+	}
+
+
+	return sorted;
+
+}
+console.log(moods);
+console.log(moods[0]);
+console.log(findRelevantMoods([1,2], [2],[2],[0]))
+
 
 
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
