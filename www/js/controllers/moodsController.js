96fa6670f05cd53eb01e3b10738a298e15c4dce1
Cameron Wheeler <cameron@scelos.com>
2016-05-02 18:56:09 -0400

    Status bar broken

--- www/js/controllers/moodsController.js
+++ www/js/controllers/moodsController.js
@@ -1,16 +1,110 @@
-app.controller('moodsController', function($scope, datastore) {
-  $scope.moods = [
-    {mood: 'Happy', time: 1},
-    {mood:'Sad', time: 2},
-    {mood:'Angry', time: 3}];
-  $scope.options = {
-    showDelete: false,
-    canSwipe: true
-  };
-  $scope.addMood = function(){
-    $scope.moods.push({mood: 'Test', time: 4});
-  };
-  $scope.editMood = function(mood){
-    mood.time += 1;
-  };
+app.controller('moodsController', function($rootScope, $scope, $location, $ionicActionSheet, $ionicPopup, $ionicScrollDelegate, $ionicPosition, datastore) {
+	$scope.go = function(path) {
+      $location.path(path);
+    }
+
+	$scope.datastore = datastore;
+	
+    $scope.moods = datastore.getAll("MoodEvent");
+
+    $scope.options = {
+        showDelete: false,
+        canSwipe: true
+    };
+
+    $scope.editMood = function(mood) {
+        mood.mood += 1;
+    };
+
+    $scope.deleteMood = function(index) {
+      datastore.removeDocument("MoodEvent", index);
+      $scope.moods = datastore.getAll("MoodEvent");
+    }
+			
+	$scope.clearFillers = function() {
+		while ($scope.moods[$scope.moods.length - 1].filler) {
+			$scope.moods.splice($scope.moods.length - 1, 1);
+		}
+		$ionicScrollDelegate.resize();
+	};
+	
+    // TODO: Fix when user holds an item and scrolls when the ActionSheet shows up moves the item to unexpected place
+    $scope.selectedIndex = -1;
+    $scope.keepSelected = false;
+	$rootScope.editMood = null;
+    $scope.moodHoldActions = function(mood, index) {
+        $scope.keepSelected = false;
+        $scope.selectedIndex = index;
+        var scrollPosition = $ionicPosition.position(angular.element(document.getElementById('id' + index)));
+        var itemHeight = document.getElementById("id" + index).offsetHeight;
+        var listHeight = angular.element(document.querySelector('#list'))[0].clientHeight;
+        while ($scope.moods.length < index + listHeight / itemHeight) {
+            $scope.moods.push({
+                mood: '',
+                filler: true
+            });
+            $scope.filledCount += 1;
+        }
+        $ionicScrollDelegate.scrollTo(0, scrollPosition.top + 1, true);
+        var $hideActions = $ionicActionSheet.show({
+            buttons: [{
+                text: 'Edit'
+            }],
+            buttonClicked: function(index) {
+                // Edit (mood)
+				$scope.keepSelected = true;
+				$rootScope.editMood = mood;
+                $hideActions();
+				$scope.go('/tab/editMood');
+            },
+            destructiveText: 'Delete',
+            destructiveButtonClicked: function() {
+                $scope.keepSelected = true;
+                $hideActions();
+                $ionicPopup.show({
+                    title: 'Are you sure?',
+                    subTitle: 'Deleting this mood will permanently remove it from the database',
+                    scope: $scope,
+                    buttons: [{
+                        text: 'Cancel',
+                        onTap: function() {
+                            $scope.moodHoldActions(mood, index);
+                        }
+                    }, {
+                        text: '<b>Confirm</b>',
+                        type: 'button-positive',
+                        onTap: function() {
+                            // Delete (mood) from datastore
+                            $scope.moods.splice(index, 1);
+							$scope.clearFillers();
+                        }
+                    }]
+                });
+            },
+            cancelText: 'Cancel',
+            cancel: function() {
+                if ($scope.keepSelected == false) {
+                    $scope.selectedIndex = -1;
+					$scope.clearFillers();
+                }
+            }
+        });
+    };
+	
+	$scope.toDateString = function(date) {
+		var monthNames = [
+		  "January", "February", "March",
+		  "April", "May", "June", "July",
+		  "August", "September", "October",
+		  "November", "December"
+		];
+		date = new Date(date);
+		var day = date.getDate();
+		var monthIndex = date.getMonth();
+		var year = date.getFullYear();
+		var hour = date.getHours() > 12 ? date.getHours() - 12 : (date.getHours() == 0 ? 12 : date.getHours());
+		var min = date.getMinutes();
+		var ampm = date.getHours() > 11 ? "PM" : "AM";
+		return monthNames[monthIndex] + " " + day + ", " + year + " " + hour + ":" + min + " " + ampm;
+	}
 });
---
+++ www/js/controllers/newMoodsController.js
@@ -0,0 +1,256 @@
+app.controller('newMoodsController', function($scope, datastore, $location, $ionicModal, $ionicPopup) {
+
+    var newLog = {
+        mood: -1,
+        level: 0,
+        trigger: -1,
+        behavior: 0,
+        belief: 0,
+        date: new Date()
+    };
+
+    $scope.doc = {
+        mood: "None",
+        level: 0,
+        trigger: "None",
+        behavior: "None",
+        belief: "None",
+        date: new Date()
+    };
+
+    $scope.modalData = {
+        header: "Header",
+        items: [{
+            name: "item 1"
+        }, {
+            name: "item2"
+        }],
+        id: 0
+    };
+
+    $scope.modalData2 = {
+        header: "Header",
+        input: "",
+        name: "",
+        isMood: false,
+        type: ""
+    };
+
+    datastore.upsert("Mood", {
+        name: "Happy",
+        type: 0
+    }, "name");
+    datastore.upsert("Mood", {
+        name: "Excited",
+        type: 1
+    }, "name");
+    datastore.upsert("Mood", {
+        name: "Tender",
+        type: 2
+    }, "name");
+    datastore.upsert("Mood", {
+        name: "Scared",
+        type: 3
+    }, "name");
+    datastore.upsert("Mood", {
+        name: "Angry",
+        type: 4
+    }, "name");
+    datastore.upsert("Mood", {
+        name: "Sad",
+        type: 5
+    }, "name");
+
+    $ionicModal.fromTemplateUrl("templates/tab-moods-new-modal.html", {
+        scope: $scope,
+        animation: "slide-in-up"
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $ionicModal.fromTemplateUrl("templates/tab-moods-new-modal2.html", {
+        scope: $scope,
+        animation: "slide-in-up"
+    }).then(function(modal) {
+        $scope.modal2 = modal;
+    });
+
+    $scope.openModal = function(id) {
+        $scope.modal.show();
+
+        switch (id) {
+            case 1:
+                $scope.modalData.header = "Select Mood";
+                $scope.modalData.id = 1;
+                $scope.modalData.items = datastore.getAll("Mood");
+                break;
+            case 2:
+                $scope.modalData.header = "Select Trigger";
+                $scope.modalData.id = 2;
+                $scope.modalData.items = datastore.getAll("Trigger");
+                break;
+            case 3:
+                $scope.modalData.header = "Select Behavior";
+                $scope.modalData.id = 3;
+                $scope.modalData.items = datastore.getAll("Behavior");
+                break;
+            case 4:
+                $scope.modalData.header = "Select Belief";
+                $scope.modalData.id = 4;
+                $scope.modalData.items = datastore.getAll("Belief");
+                break;
+            default:
+                $scope.modalData.header = "Error";
+        };
+    };
+
+    $scope.openModal2 = function() {
+        $scope.modal2.show();
+        switch ($scope.modalData.id) {
+            case 1:
+                $scope.modalData2.header = "Enter New Mood";
+                $scope.modalData2.input = "Mood: ";
+                $scope.modalData2.isMood = true;
+                break;
+            case 2:
+                $scope.modalData2.header = "Enter New Trigger";
+                $scope.modalData2.input = "Trigger: ";
+                $scope.modalData2.isMood = false;
+                break;
+            case 3:
+                $scope.modalData2.header = "Enter New Behavior";
+                $scope.modalData2.input = "Behavior: ";
+                $scope.modalData2.isMood = false;
+                break;
+            case 4:
+                $scope.modalData2.header = "Enter New Belief";
+                $scope.modalData2.input = "Belief: ";
+                $scope.modalData2.isMood = false;
+                break;
+            default:
+                $scope.modalData.header = "Error";
+        };
+    }
+
+    $scope.itemSelected = function(item) {
+        switch ($scope.modalData.id) {
+            case 1:
+                $scope.doc.mood = item.name;
+                newLog.mood = item.id;
+                $scope.modal.hide();
+                break;
+            case 2:
+                $scope.doc.trigger = item.name;
+                newLog.trigger = item.id;
+                $scope.modal.hide();
+                break;
+            case 3:
+                $scope.doc.behavior = item.name;
+                newLog.behavior = item.id;
+                $scope.modal.hide();
+                break;
+            case 4:
+                $scope.doc.belief = item.name;
+                newLog.belief = item.id;
+                $scope.modal.hide();
+                break;
+            default:
+                $scope.modal.hide();
+        };
+    }
+
+    $scope.save = function() {
+        switch ($scope.modalData.id) {
+            case 1:
+                datastore.upsert("Mood", {
+                    name: $scope.modalData2.name,
+                    type: parseInt($scope.modalData2.type)
+                }, "name");
+                $scope.modalData.items = datastore.getAll("Mood");
+                $scope.modalData2.name = "";
+                $scope.modalData2.type = "";
+                $scope.modal2.hide();
+                break;
+            case 2:
+                datastore.upsert("Trigger", {
+                    name: $scope.modalData2.name
+                }, "name")
+                $scope.modalData.items = datastore.getAll("Trigger");
+                $scope.modalData2.name = "";
+                $scope.modal2.hide();
+                break;
+            case 3:
+                datastore.upsert("Behavior", {
+                    name: $scope.modalData2.name
+                }, "name")
+                $scope.modalData.items = datastore.getAll("Behavior");
+                $scope.modalData2.name = "";
+                $scope.modal2.hide();
+                break;
+            case 4:
+                datastore.upsert("Belief", {
+                    name: $scope.modalData2.name
+                }, "name")
+                $scope.modalData.items = datastore.getAll("Belief");
+                $scope.modalData2.name = "";
+                $scope.modal2.hide();
+                break;
+            default:
+                $scope.modalData2.name = "";
+                $scope.modal2.hide();
+        };
+    }
+
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    }
+
+    $scope.closeModal2 = function() {
+        $scope.modal2.hide();
+    }
+
+    $scope.chooseMood = function(mood) {
+        $scope.doc.mood = mood.name;
+        newLog.mood = mood.id;
+    };
+
+    $scope.chooseTrigger = function(trigger) {
+        $scope.doc.trigger = trigger.name;
+        newLog.trigger = trigger.id;
+    };
+
+    $scope.chooseBehavior = function(behavior) {
+        $scope.doc.behavior = behavior.name;
+        newLog.behavior = behavior.id;
+    };
+
+    $scope.chooseBelief = function(belief) {
+        $scope.doc.belief = belief.name;
+        newLog.belief = belief.id;
+    };
+
+    $scope.addLog = function() {
+        if(newLog.mood == -1) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Mood is empty!',
+                template: 'You must choose a mood!'
+            });
+            return;
+        }
+        if(newLog.trigger == -1) {
+          var alertPopup = $ionicPopup.alert({
+              title: 'Trigger is empty!',
+              template: 'You must choose a trigger!'
+          });
+          return;
+        }
+        newLog.level = parseInt($scope.doc.level);
+        datastore.upsert("MoodEvent", newLog, "date");
+        $location.path("/tab/moods");
+    };
+
+    $scope.$on("$destroy", function() {
+        $scope.modal.remove();
+        $scope.modal2.remove();
+    });
+});
